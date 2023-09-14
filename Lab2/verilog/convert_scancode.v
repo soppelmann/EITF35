d341d032710abf8d8e95896494802c6cd8a59206
getz <getz@sdf.org>
2023-09-14 14:51:58 +0200

    all good in da h00d

--- Lab2/verilog/convert_scancode.v
+++ Lab2/verilog/convert_scancode.v
@@ -15,10 +15,10 @@ module convert_scancode
    always @ (posedge clk, negedge rst)
    if (~rst) begin
     scan_code_out <= 0;
-    valid_scan_code <= 0; 
+    valid_scan_code <= 0;
     current_counter <= 0;
-    next_counter <= 0; 
-   end else if(edge_found & (current_counter = "1010")) begin
+    next_counter <= 0;
+   end else if(edge_found & (current_counter == "1010")) begin
     scan_code[8:0] <= scan_code[9:1];
     scan_code[9] <= serial_data;
     scan_code_out[7:0] <= scan_code[7:0];
@@ -36,4 +36,3 @@ module convert_scancode
    end
 
 endmodule
-
