7c7c2a418edf6e65e83430732dba2bbc949daa75
getz <getz@sdf.org>
2023-10-02 16:35:49 +0200

    lab3 updated

--- Lab3/verilog/ALU_top.v
+++ Lab3/verilog/ALU_top.v
@@ -13,47 +13,50 @@ module ALU_top(
 
    wire d_enter;
    wire d_sign;
-   wire w_reg_ctrl;
-   wire w_func;
    wire w_sign;
    wire w_overflow;
-   wire w_result;
    wire w_bcd_digit;
-
+   wire [1:0] w_reg_ctrl;
+   wire [3:0] w_func;
+   wire [7:0] w_result;
    wire [7:0] A,B;
 
 
 
    // .modport(topport)
    debouncer enter_debouncer(
-                             .clk(clk),
-                             .rst_n(rst_n),
-                             .button_in(b_enter),
-                             .button_out(d_enter)
+                             .clk        (clk),
+                             .rst_n      (rst_n),
+                             .button_in  (b_enter),
+                             .button_out (d_enter)
                              );
 
    debouncer sign_debouncer(
-                            .clk(clk),
-                            .rst_n(rst_n),
-                            .button_in(b_sign),
-                            .button_out(d_sign)
+                            .clk        (clk),
+                            .rst_n      (rst_n),
+                            .button_in  (b_sign),
+                            .button_out (d_sign)
                             );
 
    ALU_ctrl I_ALU_CTRL(
-                       .clk(clk),
-                       .rst_n(rst_n),
-                       .enter(d_enter),
-                       .sign(d_sign),
-                       .func(w_func),
-                       .reg_ctrl(w_reg_ctrl)
+                       // Inputs
+                       .clk      (clk),
+                       .rst_n    (rst_n),
+                       .enter    (d_enter),
+                       .sign     (d_sign),
+                       // Outputs
+                       .func     (w_func),
+                       .reg_ctrl (w_reg_ctrl)
                        );
 
    reg_updater I_REG_UPDATER(
-                             .clk(clk),
-                             .rst_n(rst_n),
-                             .reg_ctrl(w_reg_ctrl),
-                             .A(A),
-                             .B (B)
+                             // Inputs
+                             .clk      (clk),
+                             .rst_n    (rst_n),
+                             .reg_ctrl (w_reg_ctrl),
+                             // Outputs
+                             .A        (A),
+                             .B        (B)
                              );
 
    ALU I_ALU (
@@ -69,23 +72,23 @@ module ALU_top(
 
 
    binary_to_bcd I_BINARY_TO_BCD (
-                                  // Outputs
-                                  .bcd_out   (w_bcd_digit),
                                   // Inputs
-                                  .binary_in (w_result)
+                                  .binary_in (w_result),
+                                  // Outputs
+                                  .bcd_out   (w_bcd_digit)
                                   );
 
 
    seven_segment_driver I_SEVEN_SEGMENT_DRIVER (
-                                                // Outputs
-                                                .digit_anode (anode),
-                                                .segment     (sev_seg),
                                                 // Inputs
                                                 .clk         (clk),
                                                 .rst_n       (rst_n),
                                                 .BCD_digit   (w_bcd_digit),
                                                 .sign        (w_sign),
-                                                .overflow    (w_overflow)
+                                                .overflow    (w_overflow),
+                                                // Outputs
+                                                .digit_anode (anode),
+                                                .segment     (sev_seg)
                                                 );
 
 
